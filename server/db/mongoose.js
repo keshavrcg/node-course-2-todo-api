f0fa054931f3f73d40588a889ee17986c461664b
Keshav <keshavkhandelwal.it.jecrc@gmail.com>
2016-12-17 15:03:58 -0800

    Setup app for heroku with mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://keshavrcg:Keshav1989@ds153677.mlab.com:53677/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
