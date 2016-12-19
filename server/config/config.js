1dc0f04fa0f8cfb5fcdded306d344ea43f47c953
Keshav <keshavkhandelwal.it.jecrc@gmail.com>
2016-12-18 18:18:12 -0800

    Add heroku MONGODB_URI

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,4 @@ if(env === 'developement'){
 } else if(env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-
-} else if(env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://keshavrcg:Keshav1989@ds153677.mlab.com:53677/todoapp';
 }
